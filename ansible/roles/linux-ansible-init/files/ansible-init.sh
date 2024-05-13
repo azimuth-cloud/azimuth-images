2421ce8e920a9efd11d5d6334bb0be86adb2a9af
Matt Pryor <matt@stackhpc.com>
2024-05-13 18:36:04 +0100

    Allow ansible-init to execute remote playbooks from metadata (#194)

--- ansible/roles/linux-ansible-init/files/ansible-init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -exo pipefail
-
-# This script executes the playbooks from /etc/ansible-init/playbooks in lexicographical order
-
-for file in $(find /etc/ansible-init/playbooks -maxdepth 1 -type f -name '*.yml' | sort -n); do
-    ansible-playbook --connection local --inventory 127.0.0.1, "$file"
-done
